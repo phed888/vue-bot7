2d2152f1c6922e6fb25ca6bfc52b140981e64b57
Brian Speight <brspeight@expedia.com>
2018-09-28 14:49:36 -0700

    Update home component to use new subsidiary components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div v-for="(group, index) in groups" :key="index" class="message-list">
+      <Message :message="message"  v-for="(message, index) in group" :key="index" />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import msg from '@/data/msg';
+import Message from '@/components/Message';
 
 export default {
   name: 'home',
+  data() {
+    return {
+      groups: msg
+    };
+  },
   components: {
-    HelloWorld
+    Message
   }
-}
+};
 </script>
+
+<style>
+body {
+  background-color: #f5f5f5;
+  margin: 0;
+  padding: 0;
+}
+.container {
+  background-color: white;
+  margin: 0 auto;
+  width: 375px;
+}
+.message-list {
+  height: 100vh;
+  overflow-y: auto;
+  overflow-x: hidden;
+  padding: 0 6px 20px;
+}
+</style>
+
