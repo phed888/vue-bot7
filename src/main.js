4b123621c8c7c54f5512b4bec96b3a969de636d5
Brian Speight <brspeight@expedia.com>
2018-09-30 15:50:08 -0700

    added vuex to yarn.lock and main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import App from './App.vue';
+import router from './router';
 
-Vue.config.productionTip = false
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    conversation: []
+  },
+  mutations: {}
+});
+
+Vue.config.productionTip = false;
 
 new Vue({
+  store,
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
