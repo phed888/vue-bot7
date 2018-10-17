0cbb6e646d2c75a457f0eec425b4fed4cc01f011
Brian Speight <brspeight@expedia.com>
2018-10-17 09:20:37 -0700

    Adding the expedia header and footer to chat bot

---
+++ src/components/ChatFooter.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="chat-footer"></div>
+</template>
+
+<script>
+export default {
+  name: 'ChatFooter',
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+.chat-footer {
+  background-image: url('../assets/text-entry.png');
+  height: 77px;
+  width: 375px;
+  position: absolute;
+  bottom: 0;
+  z-index: 3;
+}
+</style>
+
+
---
+++ src/components/ChatHeader.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="header expedia">
+    <div class="header-logo"></div>
+    <div class="header-close"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ChatHeader',
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+.header {
+  height: 26px;
+  width: 343px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 16px;
+  z-index: 5;
+  position: fixed;
+  top: 50px;
+}
+.header.expedia {
+  background-color: #ffcc02;
+}
+.expedia .header-logo {
+  background-image: url('../assets/header-expedia-logo.png');
+  height: 34px;
+  width: 118px;
+}
+.expedia .header-close {
+  background-image: url('../assets/header-close-x.png');
+  height: 18px;
+  width: 18px;
+}
+</style>
+
