9102ed9079d1d96e1effe144f569cf0db1a2260a
Brian Speight <brspeight@expedia.com>
2018-10-19 09:18:50 -0700

    adding route for new 'webview' page

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import QuickReplies from './views/QuickReplies.vue';
+import WebView from './views/WebViewPage.vue';
 
 Vue.use(Router);
 
@@ -19,6 +20,11 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ './views/CardButtons.vue')
+    },
+    {
+      path: '/web_view',
+      name: 'web_view',
+      component: WebView
     }
   ]
 });
